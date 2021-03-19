245cd29d1ee1db9864a3e36602eeb00eb0ab893e
quentinmathieu <62098792+quentinmathieu@users.noreply.github.com>
2021-03-19 23:25:44 +0100

    gitignore1

--- bin/datas.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set csgoPath=C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive
-set userID=[U:1:109876775]
-set name=autoexec.cfg
-set NumberOfLogicalProcessors=12
-set CurrentRefreshRate=144
